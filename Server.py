34c85c834109f394c554d301577f56460800e141
SapnuPuas1018 <yuvalhayun20077@gmail.com>
2023-12-12 19:07:35 +0200

    2.7(unfinished)

--- Server.py
+++ Server.py
@@ -24,6 +24,88 @@ SHORT_SIZE = 2
 FOLDER_PATH_FOR_SCREENSHOTS = r'C:\Users\nati2\OneDrive\Desktop\2.7\screen.jpg'
 
 
+def dir_func(client_socket):
+    try:
+        folder_name_bytes = Protocol.receive_(client_socket)
+        logging.debug('received: ' + folder_name_bytes)
+        print('folder_name_bytes: ' + folder_name_bytes)
+        if not os.path.exists(folder_name_bytes):
+            logging.warning('folder do not exist: ' + folder_name_bytes)
+            Protocol.send_(client_socket, 'folder do not exist: ' + folder_name_bytes)
+        else:
+            files_list = glob.glob(rf'{folder_name_bytes}*.*')
+            output = f"{files_list}"
+            logging.debug(f'the files: {output} are in: {folder_name_bytes}')
+            Protocol.send_(client_socket, output)
+    except socket.error as err:
+        Protocol.send_('received socket error while trying to dir: ' + folder_name_bytes)
+        logging.error('received socket error while trying to dir' + str(err))
+
+
+def delete_func(client_socket):
+    try:
+        file_name = Protocol.receive_(client_socket)
+        print('file_name: ' + file_name)
+        logging.debug('file name del: ' + file_name)
+        os.remove(file_name)
+        print(f"{file_name} has been removed successfully")
+        logging.debug("File deleted successfully")
+        Protocol.send_(client_socket, "File deleted successfully")
+    except OSError as err:
+        Protocol.send_(client_socket, 'received os error while trying to delete: ' + file_name)
+        logging.error('received os error while trying to delete' + str(err))
+
+    except socket.error as err:
+        Protocol.send_(client_socket, 'received socket error while trying to delete: ' + file_name)
+        logging.error('received socket error while trying to delete' + str(err))
+
+
+def copy_func(client_socket):
+    try:
+        source = Protocol.receive_(client_socket)
+        print('source: ' + source)
+        destination = Protocol.receive_(client_socket)
+        print('destination: ' + destination)
+        shutil.copy(source, destination)
+        Protocol.send_(client_socket, f"File {source} copied to {destination} successfully")
+
+    except OSError as err:
+        Protocol.send_(client_socket, f'received os error while trying to copy {source} to {destination}:')
+        logging.error(f'received os error while trying to copy {source} to {destination}:' + str(err))
+    except socket.error as err:
+        Protocol.send_(client_socket, f'received socket error while trying to copy {source} to {destination}:')
+        logging.error(f'received socket error while trying to copy {source} to {destination}:' + str(err))
+
+
+def execute_func(client_socket):
+    try:
+        program_path = Protocol.receive_(client_socket)
+        subprocess.call(program_path)
+        Protocol.send_(client_socket, 'executed successfully')
+    except OSError as err:
+        logging.error(f'received os error while trying to execute {program_path}' + str(err))
+        Protocol.send_(f'received os error while trying to execute {program_path}')
+    except socket.error as err:
+        logging.error(f'received socket error while trying to execute {program_path}' + str(err))
+        Protocol.send_(f'received socket error while trying to execute {program_path}')
+
+
+def take_screenshot_func(client_socket):
+    try:
+        image = pyautogui.screenshot()
+        image.save(FOLDER_PATH_FOR_SCREENSHOTS)
+        with open(FOLDER_PATH_FOR_SCREENSHOTS, 'rb') as image_file:
+            base64_bytes = base64.b64encode(image_file.read())
+            print(base64_bytes)
+
+            base64_string = base64_bytes.decode()
+            print(base64_string)
+            Protocol.send_(client_socket, base64_string)
+    except socket.error as err:
+        logging.error('received socket error while trying to save screenshot: ' + str(err))
+        Protocol.send_('received socket error while trying to save screenshot:')
+
+
 def return_answer(request, client_socket):
     """
     :param: request
@@ -33,57 +115,15 @@ def return_answer(request, client_socket):
     """
     print('request: ' + request)
     if request == 'DIR':
-        try:
-            folder_name_bytes = Protocol.receive_(client_socket)
-            print(folder_name_bytes)
-            logging.debug('folder_name_bytes' + folder_name_bytes)
-            x = glob.glob(rf'{folder_name_bytes}\*.*')
-            print(x)
-            output = f"{x}"
-            logging.debug("output: " + output)
-            Protocol.send_(client_socket, output)
-        except socket.error as err:
-            logging.debug('folder not found' + str(err))
+        dir_func(client_socket)
     elif request == 'DELETE':
-        try:
-            file_name = Protocol.receive_(client_socket)
-            logging.debug('file name del: ' + file_name)
-            os.remove(file_name)
-            print(f"{file_name} has been removed successfully")
-            logging.debug("File deleted successfully.")
-            Protocol.send_(client_socket, b"File deleted successfully.")
-        except socket.error as err:
-            logging.debug('file not found' + str(err))
+        delete_func(client_socket)
     elif request == 'COPY':
-        try:
-            source = Protocol.receive_(client_socket)
-            print('source: ' + source)
-            destination = Protocol.receive_(client_socket)
-            print('destination: ' + destination)
-            shutil.copy(source, destination)
-        except socket.error as err:
-            logging.error(f'received socket error while trying to copy {source} to {destination}:' + err)
+        copy_func(client_socket)
     elif request == 'EXECUTE':
-        try:
-            program_path = Protocol.receive_(client_socket)
-            print('program_path: ' + program_path)
-            subprocess.call(program_path)
-            Protocol.send_(client_socket, 'executed')
-        except socket.error as err:
-            logging.error(f'received socket error while trying to execute {program_path}' + str(err))
+        execute_func(client_socket)
     elif request == 'TAKE_SCREENSHOT':
-        try:
-            image = pyautogui.screenshot()
-            image.save(FOLDER_PATH_FOR_SCREENSHOTS)
-            with open(FOLDER_PATH_FOR_SCREENSHOTS, 'rb') as image_file:
-                base64_bytes = base64.b64encode(image_file.read())
-                print(base64_bytes)
-
-                base64_string = base64_bytes.decode()
-                print(base64_string)
-                Protocol.send_(client_socket, base64_string)
-        except socket.error as err:
-            logging.error(f'received socket error while trying to save screenshot {image}' + str(err))
+        take_screenshot_func(client_socket)
     elif request == 'EXIT':
         return 'exit'
 
@@ -100,7 +140,6 @@ def main():
             try:
                 while response != 'exit':
                     request = Protocol.receive_(client_socket)
-                    print('request: ' + request)
                     logging.debug('server received: ' + request)
                     response = return_answer(request, client_socket)
             except socket.error as err:
